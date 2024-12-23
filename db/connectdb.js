7c5da6f766d141a1e97c351f564caa99c417a33f
Javed <javedahmednizamani@gmail.com>
2024-12-23 12:41:34 +0500

    changes

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,7 +1,10 @@
 // Import mongoose first
 
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 
+const mongoURI = process.env.MONGODB_URI;
 // const connectDB = ()=>{
 //     return mongoose.connect("mongodb://localhost:27017/schooldb")
 //     .then(()=>{
@@ -12,10 +15,9 @@ import mongoose from 'mongoose'
 //     })
 // }
 
-const mongoURI = process.env.MONGODB_URI;
 const connectDB = async ()=>{
     try {
-        await mongoose.connect(mongoURI)
+        await mongoose.connect(mongoURI).then(console.log("Check URL something wrong"));
         console.log("DB connected successfully..")
     } catch (err) {
         console.log(err)
