cb4b87336b1d61cc2e9c908bf5b6e5ce8939af88
Javed <javedahmednizamani@gmail.com>
2024-12-28 17:23:03 +0500

    changes

--- controllers/cppWrongAnsRevController.js
+++ controllers/cppWrongAnsRevController.js
@@ -7,17 +7,10 @@ const question = cppDataModel;
 
 const fetchWrongAnswers = async (req, res)=>{
     try{
-        const page = parseInt(req.query.page) || 1;
-        const limit = 1;
-        const skip = (page - 1) * limit;
 
-        const questions = await question.find().skip(skip).limit(limit)
-        const totalQuestions = await question.countDocuments();
-        const totalPages = Math.ceil( totalQuestions/limit )
+        const questions = await question.find()
 
-    //    console.log(questions)
-
-       res.render('cppWrongAnsRev', {'title':'Review', questions, currentPage:page, totalPages})
+       res.render('cppWrongAnsRev', {'title':'Review', questions})
         
     }catch(error){
         console.log(error)
